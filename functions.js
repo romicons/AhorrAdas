24c74d7526bf1b09a1ebf7cdba791f359a641abc
romicons <romina.constantinoff@gmail.com>
2024-02-06 13:33:13 -0300

    Added backdrop-filter to the body background for the dark mode.

--- functions.js
+++ functions.js
@@ -1,13 +1,13 @@
-const toggleDarkModeButton = document.getElementById('btn-darkmode');
+/*    DARK/LIGHT MODE   */
 
-toggleDarkModeButton.addEventListener('click', () => {
-    const isDarkModeEnabled = document.documentElement.classList.contains('dark');
-  
-    if (isDarkModeEnabled) {
-        document.documentElement.classList.remove('dark');
-        localStorage.removeItem('theme');
-    } else {
-        document.documentElement.classList.add('dark');
-        localStorage.theme = 'dark';
-    }
-});
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+localStorage.theme = 'light';
+
+localStorage.theme = 'dark';
+
+localStorage.removeItem('theme');

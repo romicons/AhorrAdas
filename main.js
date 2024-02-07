40fb039cf56ccdd4a926b7ef2af4490f835fee68
romicons <romina.constantinoff@gmail.com>
2024-02-06 22:35:27 -0300

    Fixed an issue with the darkmode button.

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 const darkModeToggle = document.querySelector('#btn-darkmode');
 
 darkModeToggle.addEventListener('click', () => {
-document.documentElement.classList.toggle('dark');
-document.querySelector('#btn-darkmode').innerHTML = '<i class="fa-solid fa-sun"></i>';
+    document.documentElement.classList.toggle('dark');
+    if (document.documentElement.classList.contains('dark')) {
+        darkModeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    } else {
+        darkModeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    }
 });

6c945211885b4646b293d54c6845810246f2cd1e
romicons <romina.constantinoff@gmail.com>
2024-03-05 14:09:31 -0300

    Added functionality to the options of A/Z and Z/A to the filterByOrder.

--- balance-section.js
+++ balance-section.js
@@ -49,14 +49,26 @@ const filterByOrder = () => {
     const typeOperation = document.getElementById('order-operation').value;
     const savedOperations = validateLocalStorage("operations", operations);
     const orderByAmount = (a, b) => {return a.amount - b.amount}
+    const orderAlphabetycally = (a, b) => {
+        // Ordenar de la A a la Z
+        if (a.description < b.description) {
+            return -1;
+        }
+        if (a.description > b.description) {
+            return 1;
+        }
+        return 0;
+    };
+    
+
         if (typeOperation === 'Newest') {
             console.log('devuelvo la ultima operación');
             console.log(savedOperations.filter((operation) => operation.type === 'Gasto'));
-        }
+        }/*
         else if (typeOperation === 'Oldest') {
             console.log('devuelvo la operación más antigua');
             console.log(savedOperations.filter((operation) => operation.type === 'Gasto'));
-        }
+        }*/
         else if (typeOperation === 'Lowest Amount') {
             console.log('devuelvo la operación con el monto más bajo');
             savedOperations.sort(orderByAmount)
@@ -67,12 +79,14 @@ const filterByOrder = () => {
             savedOperations.sort((a, b) => orderByAmount(b, a));
             return console.log(savedOperations);
         }
-       /* else if (typeOperation === 'Z/A') {
-            console.log('devuelvo operaciones ordenadas de la Z a la A');
-            console.log(savedOperations.filter((operation) => operation.type === 'Gasto'));
-        }
         else if (typeOperation === 'A/Z') {
             console.log('devuelvo operaciones ordenadas de la A a la Z');
-            console.log(savedOperations.filter((operation) => operation.type === 'Ganancia'));
-        }*/
+            savedOperations.sort(orderAlphabetycally);
+            return console.log(savedOperations);
+        }
+        else if (typeOperation === 'Z/A') {
+            console.log('devuelvo operaciones ordenadas de la Z a la A');
+            savedOperations.sort((a, b) => orderAlphabetycally(b, a));
+            return console.log(savedOperations);
+        }
 };
